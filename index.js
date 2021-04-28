60c18043513694da81648e7f2dbcd8dde6a23a31
Nisa Kusumo <nis.kusumo@gmail.com>
2021-04-28 14:35:55 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number -= 1;
+    }
+}
+
+countDown(4);
